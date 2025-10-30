a973bc59bd83a20ec1d12bbf88c590e216e76342
300964582 <naamakisra@gmail.com>
2025-10-30 12:11:04 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -9,7 +9,7 @@ def find_max_number(num1, num2, num3):
    if num2 > num3:
      return num2
    else:
-    return num3 
+     return num3 
 
 def find_mean(num1, num2, num3):
     mean = ( num1+ num2 + num3)/3
